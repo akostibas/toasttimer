f6f08a9fcc57884583163d7f2bddd3709920385c
Alexi Kostibas <alexi@kostibas.com>
2017-11-05 15:45:18 -0800

    restyle the basic react app as a timer

--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import headerImage from './images/header.jpeg';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+        <header className="App-header" style={{
+          backgroundImage: "url('" + headerImage + "')", backgroundSize: "cover"}}>
+          <h1 className="App-title">TOAST TIMER</h1>
         </header>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          The free Toastmasters timer app.
         </p>
       </div>
     );
